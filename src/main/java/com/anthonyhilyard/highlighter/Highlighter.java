e4034803d80493158ccbf9dbf712ba304cd63e34
Anthony Hilyard <anthony.hilyard@gmail.com>
2023-04-21 12:29:29 -0700

    Fixed item coloring bug.

--- src/main/java/com/anthonyhilyard/highlighter/Highlighter.java
+++ src/main/java/com/anthonyhilyard/highlighter/Highlighter.java
@@ -172,6 +172,7 @@ public class Highlighter implements ClientModInitializer
 		RenderSystem.setShaderTexture(0, NEW_ITEM_MARKS);
 		RenderSystem.setShaderColor((color.getValue() >> 16 & 255) / 255.0f, (color.getValue() >> 8 & 255) / 255.0f, (color.getValue() & 255) / 255.0f, 1.0f);
 		Gui.blit(poseStack, x, y, HighlighterConfig.INSTANCE.useItemNameColor.get() ? 8 : 0, 0, 8, 8, 16, 16);
+		RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
 
 		poseStack.popPose();
 	}
